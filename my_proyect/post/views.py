851f21cccbb66c58bee1940be208b7ff2131aacf
fxcx <facundo19burgos@gmail.com>
2024-06-02 19:42:50 -0300

    complted post_page views posts

--- my_proyect/post/views.py
+++ my_proyect/post/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse
+
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all().order_by('-date')
+    posts = Post.objects.all().order_by("-date")
     message = "no hay post aun"
-    return render(request, 'post/post_list.html', {"posts":posts, "message":message})
+    return render(request, "post/post_list.html", {"posts": posts, "message": message})
 
 
-def  post_page(request, slug):
-     return HttpResponse(slug)
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    message = "no hay post aun"
+    return render(request, "post/post_page.html", {"post": post, "message": message})
