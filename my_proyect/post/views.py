f0ad0ee40844c900dbf6f2195a0e6d996a5c83a3
fxcx <facundo19burgos@gmail.com>
2024-06-02 19:08:35 -0300

    create views function post_page

--- my_proyect/post/views.py
+++ my_proyect/post/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'post/post_list.html', {"posts":posts})
+    posts = Post.objects.all().order_by('-date')
+    message = "no hay post aun"
+    return render(request, 'post/post_list.html', {"posts":posts, "message":message})
+
+
+def  post_page(request, slug):
+     return HttpResponse(slug)
