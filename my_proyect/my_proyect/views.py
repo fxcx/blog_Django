9983896a9a0c8f6c903c2c5257e48f2d235af019
fxcx <facundo19burgos@gmail.com>
2024-06-13 21:15:47 -0300

    render username when init session

--- my_proyect/my_proyect/views.py
+++ my_proyect/my_proyect/views.py
@@ -2,7 +2,9 @@
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "index.html")
+    # username = request.session.get('username', None)  # Obtén el nombre de usuario de la sesión
+    username = request.GET.get('username', None)  # Obtén el nombre de usuario de los parámetros de la URL
+    return render(request, "index.html", {"username":username})
 
 def about_page(request):
     return render(request, "about.html")
