e461434bf8629dd2e52cf39aade80f303b435be3
fxcx <facundo19burgos@gmail.com>
2024-06-13 21:16:12 -0300

    creation the login_user

--- my_proyect/users/views.py
+++ my_proyect/users/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from django.urls import reverse
 # Create your views here.
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            message = f'{user.username}'
-            return redirect("users:login",{"message":message})
+            login(request, form.save())
+            return redirect("users:login")
         
     form = UserCreationForm()
 
     return render(request, "login/register.html", {"form": form})
 
 
-def login(request):
-    return render(request, "login/login.html")
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # request.session['username'] = user.username  # Guarda el nombre de usuario en la sesión
+            # return  redirect("/")
+            return redirect(f"{reverse('users:home')}?username={user.username}")
+        else:
+            form = AuthenticationForm()
+            message = "Usuario o contraseña incorrectos"
+            return render(request, "login/login.html", {"form": form,"message":message})
+    
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form": form})
 
